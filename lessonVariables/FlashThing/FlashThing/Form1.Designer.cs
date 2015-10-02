07f08f050044e6f753d72647c0771cd8d3cccb6f
WookieRS <shadowpriest87@gmail.com>
2015-10-03 00:05:13 +0500

    Глава 02. Переливающийся фон. Зациклено
    while, for

---
+++ lessonVariables/FlashThing/FlashThing/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace FlashThing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGO = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonGO
+            // 
+            this.buttonGO.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonGO.Location = new System.Drawing.Point(142, 129);
+            this.buttonGO.Name = "buttonGO";
+            this.buttonGO.Size = new System.Drawing.Size(52, 45);
+            this.buttonGO.TabIndex = 0;
+            this.buttonGO.Text = "GO!";
+            this.buttonGO.UseVisualStyleBackColor = true;
+            this.buttonGO.Click += new System.EventHandler(this.buttonGO_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(287, 290);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(334, 312);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonGO);
+            this.Name = "Form1";
+            this.Text = "FlashyThing";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGO;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ lessonVariables/FlashThing/FlashThing/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlashThing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGO_Click(object sender, EventArgs e)
+        {
+            while (Visible)                                                     //После закрытия окна становится false
+            {
+                for (int c = 0; c < 255 && Visible; c++)
+                {
+                    this.BackColor = Color.FromArgb(255, 255 - c, 255 - c);
+                    Application.DoEvents();                                     //Принудительно обновляет форму. НЕ СЛЕДУЕТ ПРИМЕНЯТЬ!!
+                    System.Threading.Thread.Sleep(10);                          //Задержка на 10 мс
+                    label1.Text = "c: " + c.ToString();
+                }
+
+                for (int c = 254; c >= 0 && Visible; c--)
+                {
+                    this.BackColor = Color.FromArgb(255, 255 - c, 255 - c);
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(10);
+                    label1.Text = "c: " + c.ToString();
+                }
+            }
+                
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
