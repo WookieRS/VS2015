2b577e6eb05a4003a8569aeb54c3d96684554803
WookieRS <shadowpriest87@gmail.com>
2015-10-03 00:04:08 +0500

    Глава 02. Смена фона по нажатию.
    Дополнительно прикручен рандомный выбор цвета.

---
+++ lessonVariables/Variables/Variables/FormMain.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Variables
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private static readonly Random random = new Random(); //генерирование рандомного значения
+
+        private void button1_Click(object sender, EventArgs e)
+            
+
+
+        {
+            if (checkBox1.Checked == true)
+                
+            {
+                label1.BackColor = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255)); //каждое нажатие рандомный цвет
+                
+            } else
+            {
+                MessageBox.Show("Без галочки не буду работать!");
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- lessonVariables/Variables/Variables/Program.cs
+++ lessonVariables/Variables/Variables/Program.cs
@@ -17,7 +17,7 @@ namespace Variables
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             
-            Application.Run(new Form1());
+            Application.Run(new FormMain());
 
 
 
